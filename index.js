3daffc08df7107d510910a14a5b7ba5f2b9961b0
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-05-16 10:39:41 +0300

    Fetching data ftom s3

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+const itemKey = 'jsonReports/report_2019_05_13.json';
+const bucketName = 'teststoragereports';
+
 exports.handler = (event, context, callback) => {
     try {
-        callback(null, 'Downloader');
+        var params = {
+            Bucket: bucketName,
+            Key: keyName
+        };
+
+        s3.getObject(params, function (err, data) {
+            if (!err) {
+                console.log("Successfully got object");
+
+                callback(null, 'Downloader');
+            } else {
+                console.log(err);
+
+                throw error;
+            }
+        });
+
     } catch (error) {
         console.log('Error occured', error);
 
