ed71b684d08d7d31bba88c7bdb52d8c393e0b6bf
Andrii Maryskevych <andrii.maryskecych@gmail.com>
2019-05-16 13:58:26 +0300

    Adding extractino by id

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const AWS = require('aws-sdk');
 const jsonxml = require('jsontoxml');
 const s3 = new AWS.S3();
 
+var sql = require("mssql");
+
+var config = {
+    user: 'nodejsteam2',
+    password: 'HelloWorld!',
+    server: 'reports-db.cfgf8dz2qitj.eu-central-1.rds.amazonaws.com',
+    database: 'dbo.reports-db'
+};
+
 const itemKey = 'jsonReports/report_2019_05_13.json';
 const bucketName = 'teststoragereports';
 
@@ -20,24 +29,43 @@ exports.handler = (event, context, callback) => {
             Key: itemKey
         };
 
-        s3.getObject(params, function (err, data) {
+        const {
+            type,
+            id
+        } = event;
+
+        let query = `SELECT * FROM ReportsMetadata WHERE ID = ${id}`;
+        sql.connect(config, function(err, pool) {
             if (!err) {
-                console.log("Successfully got object");
+                pool.request().query(query, function(err, res) {
+                    if (!err) {
+                        const resopnse = res.recordset;
+                        params.Key = resopnse.S3Location;
 
-                const jsonResponse = data.Body.toString();
-                const {
-                    type,
-                    id
-                } = event;
+                        s3.getObject(params, function (err, data) {
+                            if (!err) {
+                                console.log("Successfully got object");
 
-                const serizlizer = serizlizerMap[type] || serizlizerMap.json;
-                const response = serizlizer(jsonResponse);
+                                const jsonResponse = data.Body.toString();
 
-                callback(null, response);
+                                const serizlizer = serizlizerMap[type] || serizlizerMap.json;
+                                const response = serizlizer(jsonResponse);
+
+                                callback(null, response);
+                            } else {
+                                console.log();
+
+                                callback(err);
+                            }
+                        });
+                    } else {
+                        console.log('Error fetching data from sql');
+                        callback(err);
+                    }
+                });
             } else {
-                console.log(err);
-
-                throw error;
+                console.log('Error creating SQL connection');
+                callback(err);
             }
         });
     } catch (error) {
